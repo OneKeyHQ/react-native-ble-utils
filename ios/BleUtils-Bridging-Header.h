cc7205019b16e640ac82d00ec7831d97246f0c5c
ByteZhang <ByteZhang@protonmail.com>
2025-04-07 14:08:11 +0800

    chore: init

--- ios/BleUtils-Bridging-Header.h
+++ ios/BleUtils-Bridging-Header.h
@@ -1,2 +1,7 @@
-#import <React/RCTBridgeModule.h>
-#import <React/RCTViewManager.h>
+#ifndef bleutils_Bridging_Header_h
+#define bleutils_Bridging_Header_h
+
+#import "React/RCTBridgeModule.h"
+#import "React/RCTViewManager.h"
+
+#endif /* bleutils_Bridging_Header_h */
--- ios/BleUtils.mm
+++ ios/BleUtils.mm
@@ -1,10 +1,14 @@
-#import <React/RCTBridgeModule.h>
+#import "React/RCTBridgeModule.h"
+#import <CoreBluetooth/CoreBluetooth.h>
+#import <Foundation/Foundation.h>
 
-@interface RCT_EXTERN_MODULE(BleUtils, NSObject)
+@interface RCT_EXTERN_MODULE(BleUtilsModule, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(getConnectedPeripherals:(NSArray *)serviceUUIDStrings
+                  callback:(RCTResponseSenderBlock)callback)
+
+RCT_EXTERN_METHOD(checkState:(RCTResponseSenderBlock)callback)
 
 + (BOOL)requiresMainQueueSetup
 {
